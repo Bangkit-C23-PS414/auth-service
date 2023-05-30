1c77af2b943d20e48f043fead13acedc615f34dd
Arizki Putra Rahman <arizkijitsu@gmail.com>
2023-05-30 15:21:30 +0700

    Update Profile

--- auth/profile.js
+++ auth/profile.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const admin = require('firebase-admin');
 
 const db = admin.firestore();
-
 // Route untuk mengambil data user dari Firestore
 router.get('/', async (req, res) => {
     const { email } = req.body;
