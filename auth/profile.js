9ccac478f691adafd502307f86c32a39c5ef6d8d
Arizki Putra Rahman <arizkijitsu@gmail.com>
2023-06-02 00:11:22 +0700

    Change Profile Feature

--- auth/profile.js
+++ auth/profile.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const admin = require('firebase-admin');
-
 const db = admin.firestore();
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const verify = promisify(jwt.verify);
+const { JWT_SECRET } = require('./auth');
+
 // Route untuk mengambil data user dari Firestore
 router.get('/', async (req, res) => {
-    const { email } = req.body;
+  const token = req.headers.authorization; // Ambil token dari header
 
-    try {
-        const userDoc = await db.collection('users').doc(email).get();
-        if (!userDoc.exists) {
-            return res.status(404).send('User not found');
-        }
-        
-        res.status(200).send('OK');
+  try {
+    // Decode token dan ambil email dari payload
+    const decoded = await verify(token.split(" ")[1], JWT_SECRET);
+    const email = decoded.email;
 
-    }
-    catch (error) {
-        console.error('Error fetching user data:', error);
-        res.sendStatus(500);
-    }
+    // Cari data user di Firestore berdasarkan email
+    const userDoc = await db.collection('users').doc(email).get();
+
+    // Ambil data pengguna dan kirim sebagai respons
+    const userData = userDoc.data();
+    res.send({ ...userData, password: undefined });
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
