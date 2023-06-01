c38135c3bce57635d4479f4bec9960b98ac934e8
Arizki Putra Rahman <arizkijitsu@gmail.com>
2023-06-01 20:46:35 +0700

    Change Nodemailer

--- auth/forgotpass.js
+++ auth/forgotpass.js
@@ -2,26 +2,28 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const admin = require('firebase-admin');
-
 const db = admin.firestore();
+require('dotenv').config({ path: 'SECRET_KEY.env'});
+const PASS=process.env.PASS;
 
 // Konfigurasi Nodemailer
 const transporter = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: "94a8f1160c5a54",
-    pass: "3a58183c68981c"
+    user: "wancikhaimah@gmail.com", 
+    pass: PASS
   }
 });
+
 
 // Fungsi helper untuk mengirim email
 const sendEmail = async (email, subject, message) => {
   const mailOptions = {
-    from: 'support@recoffeery.id',
     to: email,
     subject: subject,
-    text: message
+    text: message,
   };
 
   await transporter.sendMail(mailOptions);
@@ -29,7 +31,7 @@ const sendEmail = async (email, subject, message) => {
 
 // Route untuk Forget Password
 router.post('/', async (req, res) => {
-  const { email } = req.body;
+  const { email } = req.body; // Destructuring assignment
 
   try {
     const userDoc = await db.collection('users').doc(email).get();
@@ -48,7 +50,7 @@ router.post('/', async (req, res) => {
     // Send verification code via email
     const emailSubject = 'Verification Code';
     const emailMessage = `Your verification code is: ${verificationCode}`;
-
+    
     await sendEmail(email, emailSubject, emailMessage);
 
     res.sendStatus(200);
